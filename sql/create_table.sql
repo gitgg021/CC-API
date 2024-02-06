95c2b9f30ab9abe18f8d11c1ab786e4d6a17893b
caoxuehan <caoxuehan@qq.com>
2024-02-06 16:46:29 +0800

    add invokeCount 调用接口次数

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -67,3 +67,18 @@ create table user
         unique (userAccount)
 )
     comment '用户' collate = utf8mb4_unicode_ci;
+
+
+-- 用户调用接口关系表
+create table if not exists ccapi.`user_interface_info`
+(
+    `id` bigint not null auto_increment comment '主键' primary key,
+    `userId` bigint not null comment '调用用户 id',
+    `interfaceInfoId` bigint not null comment '接口 id',
+    `totalNum` int default 0 not null comment '总调用次数',
+    `leftNum` int default 0 not null comment '剩余调用次数',
+    `status` int default 0 not null comment '0-正常，1-禁用',
+    `createTime` datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    `updateTime` datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    `isDelete` tinyint default 0 not null comment '是否删除(0-未删, 1-已删)'
+) comment '用户调用接口关系';
